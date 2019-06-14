f9b7dd68f137e93f51c46ab53a9a29d3abd8e206
Adan <ajpalacios.sist@gmail.com>
2019-06-14 06:20:26 -0500

    web service

---
+++ crud-graphql/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res)=>{
+    res.send("hola mundo")
+})
+
+app.listen(8080, ()=>{
+    console.log("servidor iniciado")
+})
